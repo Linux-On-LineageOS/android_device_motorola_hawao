08e91afe0659c1463b8d61bd8cd549e828a8e9d8
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:30:35 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -70,3 +70,4 @@ PRODUCT_COPY_FILES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/hawao/hawao-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
